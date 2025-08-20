9690af669dd9a4b3823a5cea3edad7666b98c392
Alivebyte <alivebyte.yt@gmail.com>
2025-08-20 20:33:00 +0300

    Proper WinMain function

--- src/game_exe/main.cpp
+++ src/game_exe/main.cpp
@@ -1,6 +1,19 @@
-#include <iostream>
 
+#ifdef POSIX
 int main()
 {
     std::cout << "Hello world!" << std::endl;
 }
+#endif
+
+#ifdef WIN32
+#include "Windows.h"
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+{
+    
+    return EXIT_SUCCESS;
+}
+
+
+#endif
